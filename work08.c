bcc0d154412ecc685645c21fc9f245dec830ed1a
chen-grace-p4 <gchen20@stuy.edu>
2021-10-06 10:32:47 -0400

    Added headers"

---
+++ work08.c
@@ -0,0 +1,22 @@
+int mystrlen(char* s) {
+	return 0;
+}
+
+//char* mystrcpy(char* dest, char* source) {
+	
+//}
+
+//char* mystrncpy(char* dest, char*source, int n) {
+//}
+
+int mystrcmp(char* s1, char* s2) {
+	return 0;
+}
+
+//char* mystrchr(char* s, char c){
+//}
+
+int main() {
+	return 0;
+}
+
