3b4d47867c09818c3e007e2c7bb039cfbdf843a3
Grace Chen <gchen20@stuy.edu>
2021-10-06 21:53:59 -0400

    Tried some of them but not sure

--- mystring.c
+++ mystring.c
@@ -7,13 +7,13 @@ int mystrlen(char* s) {
 	return len;
 }
 
-char* mystrcpy(char* dest, char* source) {
-	int i;
-	for (i = 0; *(source+i) != '\0'; i++) {
-		*(dest+i) = *(source+i);
-	}
-	return dest;
-}
+// char* mystrcpy(char* dest, char* source) {
+// 	int i;
+// 	for (i = 0; *(source+i) != '\0'; i++) {
+// 		*(dest+i) = *(source+i);
+// 	}
+// 	return dest;
+// }
 
 char* mystrncpy(char* dest, char*source, int n) {
 	int i;
@@ -23,9 +23,26 @@ char* mystrncpy(char* dest, char*source, int n) {
 	return dest;
 }
 
+// char* mystrcat(char *dest, char*source) {
+// 	int i;
+// 	for (i = 0; *(dest+i) != '\0'; i++) {
+// 		i = i;
+// 	}
+// 	int x;
+// 	for (x = i)
+// }
+
 int mystrcmp(char* s1, char* s2) {
 	return 0;
 }
 
-//char* mystrchr(char* s, char c){
-//}
+// char* mystrchr(char* s, char c){
+// 	int i;
+// 	char* mchar;
+// 	for (i = 0; *(s+1) != '\0'; i++) {
+// 		if (*(s+i) == c) {
+// 			mchar = s+i;
+// 		}
+// 	}
+// 	return mchar;
+// }
